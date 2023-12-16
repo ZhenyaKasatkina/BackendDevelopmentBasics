d77f5b7f27559d3c5fddeec835b992ad9fbdc37d
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-12-16 15:24:11 +0300

    add def get_filtered_transactions() in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,16 @@
 import json
 
+
 def get_data_from_file(path):
     """Получение выполненных операций клиента Банка из файла"""
     with open(path) as file:
         data_file = json.load(file)
         return data_file
+
+
+def get_filtered_transactions(data_file):
+    file_executed_state = []
+    for transaction in data_file:
+        if transaction.get('state') == "EXECUTED":
+            file_executed_state.append(transaction)
+    return file_executed_state
