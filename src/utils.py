3ef7bf6142421f0cf89ed9971b2064582acb2f02
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-12-16 15:48:48 +0300

    add functions that change the format in utils.py def change_format_date() def hide_bank_account() def get_display_of_amount_and_currency()

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 
 def get_data_from_file(path):
@@ -20,3 +21,34 @@ def get_sorted_transactions(file_executed_state):
     sorted_data_file = sorted(file_executed_state, key=lambda x: x['date'], reverse=True)
     last_five_operations = sorted_data_file[:5]
     return last_five_operations
+
+
+def change_format_date(the_date):
+    """Изменение формата даты"""
+    format_date = datetime.datetime.strptime(the_date, '%Y-%m-%dT%H:%M:%S.%f')
+    return format_date.strftime('%d.%m.%Y')
+
+
+def hide_bank_account(card_account):
+    """Скрывает данные банковского счета/карты"""
+    account_client = str(card_account)
+    if "Счет" in account_client:
+        type_account = (list(str(account_client)))[:-20]
+        account = (list(str(account_client)))[-20:]
+        number_account = account[-4:]
+        return f'{"".join(type_account)} {"**"}{"".join(number_account)}'
+    else:
+        type_account = (list(str(account_client)))[:-16]
+        account = (list(str(account_client)))[-16:]
+        account[6:12] = '******'
+        chunk_size = 4
+        new_account = [account[i:i + chunk_size] for i in range(0, len(account), chunk_size)]
+        number_account = []
+        for i in new_account:
+            number_account += "".join(i) + " "
+        return f'{"".join(type_account)} {"".join(number_account).rstrip()}'
+
+
+def get_display_of_amount_and_currency(size_of_operation):
+    """Получает отображение суммы операции и денежной единицы"""
+    return f"{size_of_operation['amount']} {size_of_operation['currency']['name']}"
